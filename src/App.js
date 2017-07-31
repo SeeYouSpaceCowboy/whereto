332a50cd09535c0322883d4991d7c38e82ddef98
Mohammed Chisti <93mchisti@gmail.com>
2017-07-31 03:27:12 -0400

    Added dropdowns

--- src/App.js
+++ src/App.js
@@ -11,19 +11,52 @@ class App extends Component {
       search: {
         term: 'food',
         location: 'queens',
-        sort_by: 'rating',
-        price: 1
+        sort_by: 'best_match',
+        price: ''
       }
     }
 
     this.renderResults = this.renderResults.bind(this)
     this.handleFilterDropdown = this.handleFilterDropdown.bind(this)
     this.handlePriceDropdown = this.handlePriceDropdown.bind(this)
+    this.dropdownPick = this.dropdownPick.bind(this)
+    this.renderFilterDropdownValue = this.renderFilterDropdownValue.bind(this)
+    this.renderPriceDropdownValue = this.renderPriceDropdownValue.bind(this)
     this.handleClick = this.handleClick.bind(this)
   }
 
   handleFilterDropdown() {
     document.getElementById("search-filter-dropdown-content").classList.toggle("show");
+  }
+
+  dropdownPick(event) {
+    const field = event.target.name
+    const search = this.state.search
+    search[field] = event.target.value
+    this.setState({ search })
+
+    if(event.target.name === 'sort_by') this.handleFilterDropdown()
+    if(event.target.name === 'price') this.handlePriceDropdown()
+  }
+
+  renderFilterDropdownValue() {
+    switch(this.state.search.sort_by) {
+      case 'best_match': return 'Best Match'
+      case 'rating': return 'Rating'
+      case 'review_count': return 'Review Count'
+      case 'distance': return 'Distance'
+      default: return 'Best Match'
+    }
+  }
+
+  renderPriceDropdownValue() {
+    switch(this.state.search.price) {
+      case '1': return '$'
+      case '2': return '$$'
+      case '3': return '$$$'
+      case '4': return '$$$$'
+      default: return 'None'
+    }
   }
 
   handlePriceDropdown() {
@@ -77,22 +110,23 @@ class App extends Component {
               placeholder='Location. . .' />
 
             <div className="search-filter-dropdown-view">
-              <input onClick={ this.handleFilterDropdown } className="search-filter" value='Best Match' readonly='readonly'/>
+              <input ref='filter' onClick={ this.handleFilterDropdown } className="search-filter" value={ this.renderFilterDropdownValue() } readOnly />
               <div id="search-filter-dropdown-content" className="search-filter-dropdown-content">
-                <a href="#home">Best Match</a>
-                <a href="#about">Rating</a>
-                <a href="#contact">Review Count</a>
-                <a href="#contact">Distance</a>
+                <input name='sort_by' value='best_match' onClick={ this.dropdownPick } placeholder='Best Match' readOnly/>
+                <input name='sort_by' value='rating' onClick={ this.dropdownPick } placeholder='Rating' readOnly/>
+                <input name='sort_by' value='review_count' onClick={ this.dropdownPick } placeholder='Review Count' readOnly/>
+                <input name='sort_by' value='distance' onClick={ this.dropdownPick } placeholder='Distance' readOnly/>
               </div>
             </div>
 
             <div className="search-price-dropdown-view">
-              <input onClick={ this.handlePriceDropdown } className="search-price" value='Price' readonly='readonly'/>
+              <input ref='price' onClick={ this.handlePriceDropdown } className="search-price" value={ this.renderPriceDropdownValue() } readOnly />
               <div id="search-price-dropdown-content" className="search-price-dropdown-content">
-                <a href="#home">$</a>
-                <a href="#about">$$</a>
-                <a href="#contact">$$$</a>
-                <a href="#contact">$$$$</a>
+                <input name='price' value='' onClick={ this.dropdownPick } placeholder='none' readOnly/>
+                <input name='price' value={ 1 } onClick={ this.dropdownPick } placeholder='$' readOnly/>
+                <input name='price' value={ 2 } onClick={ this.dropdownPick } placeholder='$$' readOnly/>
+                <input name='price' value={ 3 } onClick={ this.dropdownPick } placeholder='$$$' readOnly/>
+                <input name='price' value={ 4 } onClick={ this.dropdownPick } placeholder='$$$$' readOnly/>
               </div>
             </div>
 
